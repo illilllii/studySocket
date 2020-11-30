6c2750f16611d12d4bf32d9e4a311fd16959a513
sksdl9749 <sksdl9749@gmail.com>
2020-11-30 12:11:25 +0900

    다대다 통신

---
+++ src/protocol/Chat.java
@@ -0,0 +1,8 @@
+package protocol;
+
+// 인터페이스 변수 앞에 public final static 생략가능
+public interface Chat {
+	String ALL = "ALL";	// 전체 채팅 ALL:안녕
+	String MSG = "MSG";	// 귓속말	MSG:ssar1:안녕
+	String ID = "ID";	// 아이디	ID:ssar1
+}
